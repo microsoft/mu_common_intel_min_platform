2f35196ccf51cddb9a6e4488d51ddf126653e9d9
Bret Barkelew <bret.barkelew@microsoft.com>
2021-03-22 21:14:53 -0700

    REBASE: Update CISettings branches for new CI

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -152,17 +152,17 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "release/202008"
+                "Branch": "release/202102"
             },
             {
                 "Path": "Silicon/Intel/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_silicon_intel_tiano.git",
-                "Branch": "release/202008"
+                "Branch": "release/202102"
             },
             {
                 "Path": "Common/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "release/202008"
+                "Branch": "release/202102"
             }
         ]
 
