490837545116c38bd2b7b8acd00d03e6e57e5a8b
brbarkel@microsoft.com <brbarkel@microsoft.com>
2021-09-08 17:40:58 -0700

    Update comments

--- MinPlatformPkg/Test/Library/TestPointCheckLib/DxeCheckHsti.c
+++ MinPlatformPkg/Test/Library/TestPointCheckLib/DxeCheckHsti.c
@@ -41,7 +41,7 @@ DumpHsti (
 
   SecurityFeatures = (UINT8 *) (Hsti + 1);
 
-  // MU_CHANGE [BEGIN]
+  // MU_CHANGE [BEGIN] - Don't over-index the structure if it's missing fields.
   // Handle the IHV-only field.
   if (Hsti->Role == PLATFORM_SECURITY_ROLE_PLATFORM_REFERENCE) {
     DEBUG ((DEBUG_INFO, "  SecurityFeaturesRequired    - "));
