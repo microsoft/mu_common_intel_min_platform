4c2bfa92ab2d66a56f0444ce1ec989c547cac37f
kenlautner <85201046+kenlautner@users.noreply.github.com>
2022-09-02 07:06:15 -0700

    Add Standalone MM test point support (#40)
    Adds modules, libraries, and updates existing code to support test
    points in Standalone MM.

---
+++ MinPlatformPkg/Test/Library/TestPointCheckLib/StandaloneMmCheckMemoryAttribute.c
@@ -0,0 +1,66 @@
+/** @file
+Memory Attribute table test for Standalone MM.
+
+Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <PiMm.h>
+#include <Library/MmServicesTableLib.h>
+#include <Library/TestPointCheckLib.h>
+#include <Library/TestPointLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiLib.h>
+#include <Library/PrintLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Guid/MemoryAttributesTable.h>
+#include <Guid/PiSmmMemoryAttributesTable.h>
+#include <Protocol/LoadedImage.h>
+
+VOID
+TestPointDumpMemoryAttributesTable (
+  IN EFI_MEMORY_ATTRIBUTES_TABLE                     *MemoryAttributesTable
+  )
+{
+  UINTN                 Index;
+  EFI_MEMORY_DESCRIPTOR *Entry;
+  UINT64                Pages[EfiMaxMemoryType];
+
+  ZeroMem (Pages, sizeof(Pages));
+
+  DEBUG ((DEBUG_INFO, "MemoryAttributesTable:"));
+  DEBUG ((DEBUG_INFO, " Version=0x%x", MemoryAttributesTable->Version));
+  DEBUG ((DEBUG_INFO, " Count=0x%x", MemoryAttributesTable->NumberOfEntries));
+  DEBUG ((DEBUG_INFO, " DescriptorSize=0x%x\n", MemoryAttributesTable->DescriptorSize));
+  Entry = (EFI_MEMORY_DESCRIPTOR *)(MemoryAttributesTable + 1);
+  DEBUG ((DEBUG_INFO, "Type       Start            End              # Pages          Attributes\n"));
+  for (Index = 0; Index < MemoryAttributesTable->NumberOfEntries; Index++) {
+    DEBUG ((DEBUG_INFO, " %016LX-%016LX %016LX %016LX\n",
+      Entry->PhysicalStart,
+      Entry->PhysicalStart+MultU64x64 (SIZE_4KB,Entry->NumberOfPages) - 1,
+      Entry->NumberOfPages,
+      Entry->Attribute
+      ));
+    if (Entry->Type < EfiMaxMemoryType) {
+      Pages[Entry->Type] += Entry->NumberOfPages;
+    }
+    Entry = NEXT_MEMORY_DESCRIPTOR (Entry, MemoryAttributesTable->DescriptorSize);
+  }
+  
+  DEBUG ((DEBUG_INFO, "\n"));
+  DEBUG ((DEBUG_INFO, "  RT_Code   : %14ld Pages (%ld Bytes)\n", Pages[EfiRuntimeServicesCode],     MultU64x64(SIZE_4KB, Pages[EfiRuntimeServicesCode])));
+  DEBUG ((DEBUG_INFO, "  RT_Data   : %14ld Pages (%ld Bytes)\n", Pages[EfiRuntimeServicesData],     MultU64x64(SIZE_4KB, Pages[EfiRuntimeServicesData])));
+  DEBUG ((DEBUG_INFO, "              -------------- \n"));
+}
+
+EFI_STATUS
+TestPointCheckStandaloneMmMemAttribute (
+  VOID
+  )
+{
+  // Unsupported by current implementation of Standalone MM
+  return EFI_UNSUPPORTED;
+}
