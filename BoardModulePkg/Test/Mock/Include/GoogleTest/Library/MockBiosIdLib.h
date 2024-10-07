d0968778a966e1ff43e1e15ef991404760922bd1
Vivian Nowka-Keane <vnowkakeane@linux.microsoft.com>
2024-10-07 11:03:26 -0700

    [CHERRY-PICK] BoardModulePkg: Added Mock library for BiosIdLib (#301)
    ## Description
    Added a gmock for GoogleTests that pull in BiosIdLib.
    
    Cherry picked from edk2_platforms
    
    [c53db4dc8bbc6cec6016792b9e08b1f0630498ae](https://github.com/tianocore/edk2-platforms/commit/c53db4dc8bbc6cec6016792b9e08b1f0630498ae)
    Amended to add newline in header file description.
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    Consumed mock in GoogleTest
    
    ## Integration Instructions
    N/A
    
    Signed-off-by: Vivian Nowka-Keane <vnowkakeane@linux.microsoft.com>
    Co-authored-by: Bhavani Subramanian <v-bhavanisu@microsoft.com>

---
+++ BoardModulePkg/Test/Mock/Include/GoogleTest/Library/MockBiosIdLib.h
@@ -0,0 +1,41 @@
+/** @file MockBiosIdLib.h
+  Google Test mocks for BiosIdLib
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef MOCK_BIOS_ID_LIB_H_
+#define MOCK_BIOS_ID_LIB_H_
+
+#include <Library/GoogleTestLib.h>
+#include <Library/FunctionMockLib.h>
+extern "C" {
+  #include <Uefi.h>
+  #include <Pi/PiBootMode.h>
+  #include <Library/BiosIdLib.h>
+}
+
+struct MockBiosIdLib {
+  MOCK_INTERFACE_DECLARATION (MockBiosIdLib);
+
+  MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
+    GetBiosId,
+    (
+     OUT BIOS_ID_IMAGE     *BiosIdImage OPTIONAL
+    )
+    );
+
+  MOCK_FUNCTION_DECLARATION (
+    EFI_STATUS,
+    GetBiosVersionDateTime,
+    (
+     OUT CHAR16    *BiosVersion, OPTIONAL
+     OUT CHAR16    *BiosReleaseDate, OPTIONAL
+     OUT CHAR16    *BiosReleaseTime OPTIONAL
+    )
+    );
+};
+
+#endif
