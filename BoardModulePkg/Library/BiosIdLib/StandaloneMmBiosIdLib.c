913bb7aac367c3a225b4c1d36f5262f779ac10df
Lixia Huang <lisa.huang@intel.com>
2024-10-17 10:42:13 -0700

    [Cherry-Pick] Add Standalone Mm BiosIdLib and format code with Uncrustify.
    Cc: Eric Dong <eric.dong@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    
    Signed-off-by: Lixia Huang <lisa.huang@intel.com>

--- BoardModulePkg/Library/BiosIdLib/StandaloneMmBiosIdLib.c
+++ BoardModulePkg/Library/BiosIdLib/StandaloneMmBiosIdLib.c
@@ -1,28 +1,25 @@
 /** @file
-  Boot service DXE BIOS ID library implementation.
+  Boot service StandaloneMm BIOS ID library implementation.
 
   These functions in this file can be called during DXE and cannot be called during runtime
   or in SMM which should use a RT or SMM library.
 
 
-Copyright (c) 2015 - 2023, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-#include <PiDxe.h>
-#include <Library/BaseLib.h>
+#include <PiMm.h>
 #include <Library/HobLib.h>
-#include <Library/DxeServicesLib.h>
 #include <Library/BaseMemoryLib.h>
-#include <Library/MemoryAllocationLib.h>
 #include <Library/DebugLib.h>
 #include <Library/BiosIdLib.h>
 
 #include <Guid/BiosId.h>
 
 /**
-  This function returns BIOS ID by searching HOB or FV.
+  This function returns BIOS ID by searching HOB.
   It also debug print the BIOS ID found.
 
   @param[out] BiosIdImage   The BIOS ID got from HOB or FV. It is optional,
@@ -38,7 +35,6 @@ GetBiosId (
   OUT BIOS_ID_IMAGE  *BiosIdImage OPTIONAL
   )
 {
-  EFI_STATUS     Status;
   BIOS_ID_IMAGE  TempBiosIdImage;
   VOID           *Address;
   UINTN          Size;
@@ -59,34 +55,11 @@ GetBiosId (
     Size = sizeof (BIOS_ID_IMAGE);
     CopyMem ((VOID *)BiosIdImage, GET_GUID_HOB_DATA (Address), Size);
 
-    DEBUG ((DEBUG_INFO, "DXE get BIOS ID from HOB successfully\n"));
+    DEBUG ((DEBUG_INFO, "StandaloneMm get BIOS ID from HOB successfully\n"));
     DEBUG ((DEBUG_INFO, "BIOS ID: %s\n", (CHAR16 *)(&(BiosIdImage->BiosIdString))));
     return EFI_SUCCESS;
   }
 
-  Status = GetSectionFromAnyFv (
-             &gBiosIdGuid,
-             EFI_SECTION_RAW,
-             0,
-             &Address,
-             &Size
-             );
-  if ((Status == EFI_SUCCESS) && (Address != NULL)) {
-    //
-    // BIOS ID image is present in FV.
-    //
-    Size = sizeof (BIOS_ID_IMAGE);
-    CopyMem ((VOID *)BiosIdImage, Address, Size);
-    //
-    // GetSectionFromAnyFv () allocated buffer for Address, now free it.
-    //
-    FreePool (Address);
-
-    DEBUG ((DEBUG_INFO, "DXE get BIOS ID from FV successfully\n"));
-    DEBUG ((DEBUG_INFO, "BIOS ID: %s\n", (CHAR16 *)(&(BiosIdImage->BiosIdString))));
-    return EFI_SUCCESS;
-  }
-
-  DEBUG ((DEBUG_ERROR, "DXE get BIOS ID failed: %r\n", EFI_NOT_FOUND));
+  DEBUG ((DEBUG_ERROR, "StandaloneMm get BIOS ID failed: %r\n", EFI_NOT_FOUND));
   return EFI_NOT_FOUND;
 }
