37b891eac58e95a2d29d47eb5d5cdee158abd57d
PaddyDeng <v-paddydeng@microsoft.com>
2024-10-29 11:09:55 +0800

    Minor fixes for #224

--- MinPlatformPkg/PlatformInit/PlatformInitPei/FspSupport.c
+++ MinPlatformPkg/PlatformInit/PlatformInitPei/FspSupport.c
@@ -1,3 +1,10 @@
+/** @file FspSupport.c
+  Provides FSP mode selection value based on PcdFspModeSelection
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
 #include <PiPei.h>
 
 UINT8
--- MinPlatformPkg/PlatformInit/PlatformInitPei/FspSupportNull.c
+++ MinPlatformPkg/PlatformInit/PlatformInitPei/FspSupportNull.c
@@ -1,3 +1,10 @@
+/** @file FspSupportNull.c
+  Provides dummy FSP mode selection value that will always be 0.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
 #include <PiPei.h>
 
 UINT8
